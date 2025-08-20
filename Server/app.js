a591822ce9dfc925f1cce000ee1c348c702ae1c6
dhanunjaya5058546@gmail.com <dhanunjaya5058546@gmail.com>
2025-08-20 08:17:18 +0000

    Added

---
+++ Server/app.js
@@ -0,0 +1,18 @@
+import bcrypt from "bcrypt";
+
+const password = "Riyaz@000";
+
+
+bcrypt.hash(password, 10, (err, hash) => {
+   
+  if (err) throw err;
+  console.log("Hashed Password:", hash);
+
+
+  bcrypt.compare(password, hash, (err, result) => {
+    console.log("Password Match:", result);
+
+  });
+});
+
+
